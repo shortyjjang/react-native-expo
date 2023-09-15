70ca790fbbdad33d5d1860a044f9506c769cac45
kimmiran <kimmiran@a2dcorp.co.kr>
2023-09-15 13:59:35 +0900

    move landing to main with params

--- App.js
+++ App.js
@@ -10,7 +10,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Landing" component={PageLanding} />
+        <Stack.Screen name="Landing" 
+        options={
+          {headerShown: false}
+        }
+        component={PageLanding} />
         <Stack.Screen name="Main" component={PageMain} />
       </Stack.Navigator>
     </NavigationContainer>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import tw from 'twrnc';
 
 
-export default function PageMain() {
+export default function PageLanding({ navigation}) {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('Main');
+    }, 2000);
+  },[]);
     return (
       <View style={tw`flex-1 bg-green-500 items-center justify-center`}>
         <Text style={tw`text-white`}>Landing!</Text>
